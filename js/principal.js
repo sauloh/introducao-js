fda4404a15392f71c7a9a6b23b30a2d1aed44ed6
sauloh <saulo.hachem@gmail.com>
2017-12-04 23:28:11 -0300

    Calculate  IMC and validate height and weight

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,30 @@
-console.log("Fui carregado de um arquivo externo");
-
 var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista";
+
+var altura_error = "Altura invalida;";
+var peso_error = "Peso invalido";
+var imc_value = 0;
+var error = false;
+
+var paciente = document.querySelector("#primeiro-paciente"); // Get pacient
+var peso = paciente.querySelector(".info-peso").textContent; // Get weigth
+var altura = paciente.querySelector(".info-altura").textContent; // Get height
+var tdimc = paciente.querySelector(".info-imc");
+
+tdimc.textContent = '';
+
+if (altura <= 0 || altura >= 3) {
+    tdimc.textContent = altura_error;
+    error = true;
+}
+
+if (peso <= 0 || peso >= 1000) {
+    tdimc.textContent += peso_error;
+    error = true;
+}
+
+if (!error) {
+    imc_value = peso / (altura * altura); // Calculates IMC
+    tdimc.textContent = imc_value; // Update IMC value
+}
