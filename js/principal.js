50abea51dd23e041ae5ae807839bec12c925cfef
sauloh <saulo.hachem@gmail.com>
2017-12-06 13:07:39 -0300

    Adding calc for all pacients; fix 2 decimal cases; color invalid data

--- js/principal.js
+++ js/principal.js
@@ -4,27 +4,36 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var altura_error = "Altura invalida;";
 var peso_error = "Peso invalido";
-var imc_value = 0;
-var error = false;
 
-var paciente = document.querySelector("#primeiro-paciente"); // Get pacient
-var peso = paciente.querySelector(".info-peso").textContent; // Get weigth
-var altura = paciente.querySelector(".info-altura").textContent; // Get height
-var tdimc = paciente.querySelector(".info-imc");
 
-tdimc.textContent = '';
+var pacient_list = document.querySelectorAll(".paciente"); // Get pacient
 
-if (altura <= 0 || altura >= 3) {
-    tdimc.textContent = altura_error;
-    error = true;
-}
+for (var i=0; i<pacient_list.length; i++) {
+    var paciente = pacient_list[i];
+    var imc_value = 0;
+    var error = false;
+    var peso = paciente.querySelector(".info-peso").textContent; // Get weigth
+    var altura = paciente.querySelector(".info-altura").textContent; // Get height
+    var tdimc = paciente.querySelector(".info-imc");
 
-if (peso <= 0 || peso >= 1000) {
-    tdimc.textContent += peso_error;
-    error = true;
-}
+    tdimc.textContent = '';
 
-if (!error) {
-    imc_value = peso / (altura * altura); // Calculates IMC
-    tdimc.textContent = imc_value; // Update IMC value
+    if (altura <= 0 || altura >= 3) {
+        tdimc.textContent = altura_error;
+        error = true;
+    }
+
+    if (peso <= 0 || peso >= 1000) {
+        tdimc.textContent += peso_error;
+        error = true;
+    }
+
+    if (!error) {
+        imc_value = peso / (altura * altura); // Calculates IMC
+        tdimc.textContent = imc_value.toFixed(2); // Update IMC value
+    }
+
+    else{
+        paciente.classList.add("invalid-pacient")
+    }
 }
