8004f8f465175a99cf29703507c85e600322fa6d
sauloh <saulo.hachem@gmail.com>
2017-12-09 23:00:24 -0300

    Adding  new pacients to table on mouse click

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,8 @@
-var titulo = document.querySelector(".titulo");
+var titulo = document.querySelector(".titulo"); // Get Title
 
-titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "Aparecida Nutricionista"; // Update Title
 
+// Error Messages
 var altura_error = "Altura invalida;";
 var peso_error = "Peso invalido";
 
@@ -18,22 +19,77 @@ for (var i=0; i<pacient_list.length; i++) {
 
     tdimc.textContent = '';
 
+    // Height check
     if (altura <= 0 || altura >= 3) {
         tdimc.textContent = altura_error;
         error = true;
     }
 
+    // Weight check
     if (peso <= 0 || peso >= 1000) {
         tdimc.textContent += peso_error;
         error = true;
     }
 
+    // Print if errors
     if (!error) {
         imc_value = peso / (altura * altura); // Calculates IMC
         tdimc.textContent = imc_value.toFixed(2); // Update IMC value
     }
 
+    // Print if Ok
     else{
         paciente.classList.add("invalid-pacient")
     }
 }
+
+// +++ Buttom Function - Adding new pacient +++ //
+var addButtom = document.querySelector("#adicionar-paciente");
+
+// Event on Click
+addButtom.addEventListener("click", function () {
+    event.preventDefault();
+
+    // Get input form data
+    var form = document.querySelector("#form-adiciona");
+    var name = form.nome.value;
+    var weight = form.peso.value;
+    var height = form.altura.value;
+    var fat = form.gordura.value;
+
+    // Creating elements of a new row in table
+    var pacientTr = document.createElement("tr");
+    pacientTr.classList.add("paciente");
+
+    var nameTd = document.createElement("td");
+    nameTd.classList.add("info-nome");
+
+    var weightTd = document.createElement("td");
+    weightTd.classList.add("info-peso");
+
+    var heightTd = document.createElement("td");
+    heightTd.classList.add("info-altura");
+
+    var fatTd = document.createElement("td");
+    fatTd.classList.add("info-gordura");
+
+    var imcTd = document.createElement("td");
+
+    nameTd.textContent = name;
+    weightTd.textContent = weight;
+    heightTd.textContent = height;
+    fatTd.textContent = fat;
+
+    pacientTr.appendChild(nameTd);
+    pacientTr.appendChild(weightTd);
+    pacientTr.appendChild(heightTd);
+    pacientTr.appendChild(fatTd);
+    pacientTr.appendChild(imcTd);
+
+    var pacientTable= document.querySelector("#tabela-pacientes");
+
+    pacientTable.appendChild(pacientTr);
+
+    console.log(pacientTr);
+
+})
